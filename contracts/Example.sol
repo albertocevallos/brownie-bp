c177e320bacfd9bb5125e0a435667fcfa4f6fb41
Alberto Cevallos <acvllsh@gmail.com>
2022-02-23 16:31:28 -0500

    added example contract

---
+++ contracts/Example.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.12;
+
+import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+/**
+ * @title Example
+ * @author Vitalik Buterin
+ *
+ * Example contract description
+ *
+ */
+contract MetaGovernance {
+
+    using SafeMath for uint256;
+
+    /* ============ Enums ============ */
+    /* ============ Constants ============= */
+    /* ============ State Variables ============ */
+
+    /* ============ Events ============ */
+    /* ============ Modifiers ============ */
+
+    /* ============ Constructor ============ */
+    constructor(
+    ) {
+    }
+
+    /* ============ External Functions ============ */
+
+    /* ============ Internal Functions ============ */
+}
